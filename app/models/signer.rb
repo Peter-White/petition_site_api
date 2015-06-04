7a1f7d121b46f9c19c781a0598c9585f4bcb2c36
Peter-White <pwhite007@icloud.com>
2015-06-04 12:26:40 -0400

    Validations Added

--- app/models/signer.rb
+++ app/models/signer.rb
@@ -1,2 +1,5 @@
 class Signer < ActiveRecord::Base
+  validates :first_name, :last_name, :age, :email, :address, presence: true
+  validates :email, uniqueness: true
+  validates_numericality_of :age, :only_integer => true, :greater_than_or_equal_to => 18
 end
